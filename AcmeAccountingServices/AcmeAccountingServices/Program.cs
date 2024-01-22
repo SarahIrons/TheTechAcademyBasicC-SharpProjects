732fe3f4abd5e635c7cec309ccca7e85b053f919
SarahIrons <141857424+SarahIrons@users.noreply.github.com>
2024-01-22 15:58:58 -0500

    Add files via upload

---
+++ AcmeAccountingServices/AcmeAccountingServices/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AcmeAccountingServices
+{
+    class Program
+    {
+        private static int Arg0Arg1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Services");
+            Console.WriteLine(@"Remember, we're ""accounting"" on you!");
+
+
+        }
+    }
+}
