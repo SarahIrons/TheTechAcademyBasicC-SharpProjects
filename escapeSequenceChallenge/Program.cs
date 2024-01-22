b3538e52fd3b179cf4650cd4b4c619e2588bf63b
SarahIrons <141857424+SarahIrons@users.noreply.github.com>
2024-01-22 14:00:40 -0500

    Add files via upload

---
+++ escapeSequenceChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace escapeSequenceChallenge
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi,\n");
+            Console.WriteLine("\n do you like apples?");
+            Console.Write(Console.ReadLine());
+            Console.Write(Console.ReadLine());
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
